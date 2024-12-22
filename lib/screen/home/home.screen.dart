5485a8fe6c6613a8daa05491b0db5a2126801800
bogs212000 <garryearllontes21@gmail.com>
2024-12-22 16:42:26 +0800

    first commit

---
+++ lib/screen/home/home.screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: VxBox()
+          .height(MediaQuery.of(context).size.height)
+          .width(MediaQuery.of(context).size.width)
+          .white
+          .make(),
+    );
+  }
+}
